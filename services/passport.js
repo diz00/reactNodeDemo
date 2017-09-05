99fc94acd0ac12b481749e9a54743979df9e1ffe
Diz00 <valkinvalero@gmail.com>
2017-09-05 14:34:58 +0300

    fixed bs

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,16 @@ passport.use(
         callbackURL: '/auth/google/callback',
         proxy: true
     }, 
-    (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId: profile.id })
-            .then((existingUser) => {
-                if (existingUser) {
-                    // we already have a record with the given profile ID
-                    done(null, existingUser);
-                } else {
-                    // we don't have a user record with this ID, make a new record
-                    new User({ googleId: profile.id })
-                        .save()
-                        .then(user => done());
-                }
-            });
+    async (accessToken, refreshToken, profile, done) => {
+        let existingUser = await User.findOne({ googleId: profile.id })
+        
+        if (existingUser) {
+            return done(null, existingUser);
+        }
+        
+        let user = await new User({ googleId: profile.id }).save();
+        done(null, user);
+        
+        
     })
 );
